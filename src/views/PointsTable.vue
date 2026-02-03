cad9f33d18963c95bd8b245e58e5dd7ed95a19bc
aroyofficial <aroy02072000@gmail.com>
2026-02-03 20:38:46 +0530

    Design points table

--- src/views/PointsTable.vue
+++ src/views/PointsTable.vue
@@ -1,38 +1,69 @@
 <template>
-	<div id="points-table-wrapper">
-		<div id="points-table-filters" class="mb-3">
-			<el-select
-				v-model="selectedSeason"
-				placeholder="Select Season"
-				style="width: 180px"
-				size="large"
-				:popper-options="dropdownPopperOptions"
-				@change="fetchLeagueTable"
-			>
-				<el-option
-					v-for="item in seasons"
-					:key="item.id"
-					:label="item.name"
-					:value="item.id"
-				/>
-			</el-select>
-		</div>
-		<div>
-			<el-table
-				v-if="
-					leagueTable &&
-					leagueTable.standings &&
-					leagueTable.standings.length > 0
-				"
-				:data="leagueTable.standings"
-				style="width: 100%"
-			>
-				<el-table-column prop="team_name" label="TEAM" width="180" />
-				<el-table-column prop="matches_played" label="P" width="180" />
-				<el-table-column prop="wins" label="W" />
-				<el-table-column prop="net_points" label="NET" />
-				<el-table-column prop="points" label="PTS" />
-			</el-table>
+	<div
+		id="points-table-wrapper"
+		class="d-flex flex-column align-items-center mt-5"
+	>
+		<div class="d-flex flex-column" style="width: max-content">
+			<div id="points-table-filters" class="mb-4 d-flex justify-content-end">
+				<el-select
+					v-model="selectedSeason"
+					placeholder="Select Season"
+					style="width: 180px"
+					size="large"
+					:popper-options="dropdownPopperOptions"
+					@change="fetchLeagueTable"
+				>
+					<el-option
+						v-for="item in seasons"
+						:key="item.id"
+						:label="item.name"
+						:value="item.id"
+					/>
+				</el-select>
+			</div>
+			<div>
+				<el-table
+					v-if="
+						leagueTable &&
+						leagueTable.standings &&
+						leagueTable.standings.length > 0
+					"
+					:data="leagueTable.standings"
+					style="width: fit-content"
+				>
+					<el-table-column prop="team_name" label="TEAM" width="180" />
+					<el-table-column
+						prop="matches_played"
+						label="P"
+						width="80"
+						class="text-center"
+					/>
+					<el-table-column
+						prop="wins"
+						label="W"
+						width="80"
+						class="text-center"
+					/>
+					<el-table-column
+						prop="losses"
+						label="L"
+						width="80"
+						class="text-center"
+					/>
+					<el-table-column
+						prop="net_points"
+						label="NET"
+						width="100"
+						class="text-center"
+					/>
+					<el-table-column
+						prop="points"
+						label="PTS"
+						width="100"
+						class="text-center"
+					/>
+				</el-table>
+			</div>
 		</div>
 	</div>
 </template>
@@ -59,6 +90,16 @@ const fetchLeagueTable = async () => {
 	seasonStore.setSeason(selectedSeason.value);
 	await seasonStore.fetchLeagueTable();
 	leagueTable.value = seasonStore.leagueTable;
+	if (
+		leagueTable.value &&
+		leagueTable.value.standings &&
+		leagueTable.value.standings.length > 0
+	) {
+		leagueTable.value.standings[0].team_name += " 🏆";
+		leagueTable.value.standings.forEach((team, index) => {
+			team.losses = team.matches_played - team.wins;
+		});
+	}
 };
 
 onMounted(async () => {
@@ -69,7 +110,12 @@ onMounted(async () => {
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+::v-deep(.el-table__header-wrapper th.el-table__cell:not(:first-child) > .cell),
+::v-deep(.el-table__row > td.el-table__cell:not(:first-child) > .cell) {
+	text-align: center;
+}
+
 .el-select__wrapper.is-focused {
 	box-shadow: none !important;
 }
